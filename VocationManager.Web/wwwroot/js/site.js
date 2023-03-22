05c9c628a79ad2abadd17790fbfbb5a3840abcc5
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2023-03-22 22:24:56 +0200

    Изнесен общ код за добавяне на обект към екип

--- VocationManager.Web/wwwroot/js/site.js
+++ VocationManager.Web/wwwroot/js/site.js
@@ -1,4 +1,63 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function parseTeamInputs(teamsSelector) {
+    var availableTeams = $(teamsSelector).val();
 
-// Write your JavaScript code.
+    var teamInputs = {};
+    if (!availableTeams.includes("#")) {
+        let teamTokens = availableTeams.split("+=+");
+        teamInputs[teamTokens[0]] = teamTokens[1];
+    } else {
+        for (var team of availableTeams.split("#")) {
+            let teamTokens = team.split("+=+");
+
+            teamInputs[teamTokens[0]] = teamTokens[1];
+        }
+    }
+
+    return teamInputs;
+}
+
+function startAssigningToTeam(teamInputs, model, endpoint) {
+    Swal.fire({
+        title: 'Select a team',
+        input: 'select',
+        inputOptions: teamInputs,
+        inputPlaceholder: 'Select a team',
+        showCancelButton: true,
+        inputValidator: (value) => {
+
+            if (value == "") {
+                return Swal.fire(
+                    'Error!',
+                    'Please choose a team!',
+                    'error'
+                );
+            } else {
+                return new Promise((resolve) => {
+                    model.teamId = value;
+                    $.ajax({
+                        type: "POST",
+                        url: endpoint,
+                        data: model,
+                        dataType: "json",
+                        success: function (response) {
+                            Swal.fire(
+                                'Success!',
+                                'User was successfully assigned to the team!',
+                                'success'
+                            ).then(() => window.location.reload());
+                        },
+                        error: function (event, jqxhr, settings, thrownError) {
+                            if (event.status != 200) {
+                                Swal.fire(
+                                    'Error!',
+                                    event.responseText.split(': ')[1].split("\r\n")[0],
+                                    'error'
+                                );
+                            }
+                        }
+                    });
+                });
+            }
+        }
+    });
+}
