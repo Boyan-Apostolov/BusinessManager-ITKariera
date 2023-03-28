1e8a64cf6c7bdfc989c8db45cb468dadd7d00ad5
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2023-03-28 21:30:55 +0300

    Отпуски -> Създаване и Редакция -> Възможност за скриване на част от инпутите в зависимост от избора в модала

---
+++ BusinessManager.Web/wwwroot/js/timeOff-validations.js
@@ -0,0 +1,49 @@
+﻿let halfDayHolder = $('.half-day-holder');
+
+let toDateHolder = $('.to-date-holder');
+let toDate = $('#To');
+
+let fileHolder = $('.file-holder-holder');
+
+$('#Type').on('change',
+    function () {
+        let selectedVal = $(this).val();
+
+        if (selectedVal == "Sick") {
+            halfDayHolder.hide();
+            $('#IsHalfDay').prop('checked', false);
+            toDateHolder.show();
+
+            fileHolder.show();
+        } else {
+            halfDayHolder.show();
+            fileHolder.hide();
+        }
+    });
+
+$('#IsHalfDay').on('click', isHalfDayCheck);
+
+function isHalfDayCheck() {
+    if ($('#IsHalfDay').is(":checked")) {
+        toDateHolder.hide();
+    } else {
+        toDateHolder.show();
+    }
+}
+
+$('#submit-btn').on('click',
+    function (e) {
+        if (!toDate.val() && toDate.is(":visible")) {
+            e.preventDefault();
+            Swal.fire(
+                'Error!',
+                "'To date' is required!",
+                'error'
+            );
+        }
+    });
+
+$(function () {
+    $('#Type').trigger('change');
+    isHalfDayCheck();
+})
